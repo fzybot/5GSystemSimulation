38157fa36d182c0ddc4b9660a007bfe67ff87667
fzybot <ruslan.akhpashev@gmail.com>
2020-11-12 14:50:20 +0700

    just changes

--- src/equipment/BaseStation.h
+++ src/equipment/BaseStation.h
@@ -1,4 +1,17 @@
 #ifndef BASESTATION_H
 #define BASESTATION_H
 
+#include <src/equipment/Equipment.h>
+
+
+class BaseStation : public Equipment{
+public:
+
+    // Logistic information
+    int PCI;
+    int cellIdentity;
+
+};
+
+
 #endif // BASESTATION_H
--- src/equipment/UserEquipment.h
+++ src/equipment/UserEquipment.h
@@ -1,4 +1,21 @@
 #ifndef USEREQUIPMENT_H
 #define USEREQUIPMENT_H
 
+#include <src/equipment/Equipment.h>
+
+
+class UserEquipment : public Equipment{
+public:
+
+    // Logistic information
+    int identity;
+
+};
+
+
+
+
+
+
+
 #endif // USEREQUIPMENT_H
