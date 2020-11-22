16b8a9486160e86f1752b8493b2bc4a225c13ad0
fzybot <ruslan.akhpashev@gmail.com>
2020-11-22 23:12:38 +0700

    testing

---
+++ src/antenna/antenna.h
@@ -0,0 +1,29 @@
+#ifndef ANTENNA_H
+#define ANTENNA_H
+
+/* Antenna modeling by TR 38.901 (page 22)
+ *
+ *
+ */
+
+
+class Antenna{
+
+public:
+
+    int gain;
+    int type;
+
+public:
+    Antenna(){
+
+    }
+
+    void setGain(int g){
+        this->gain = g;
+    }
+};
+
+
+
+#endif // ANTENNA_H
---
+++ src/equipment/equipment.h
@@ -0,0 +1,68 @@
+#ifndef EQUIPMENT_H
+#define EQUIPMENT_H
+
+#include <src/common_parameters.h>
+
+class Equipment {
+
+public:
+
+    /* Coordinates & Geometry for spherical basis
+     * According to 3GPP TS38.901:
+     *  latitude is 'y' axis
+     *  lontitude is 'x' axis
+     *  altitude is  'z' axis (height above the sea)
+     *
+     *  angles:
+     *      lonAngle - 'fi'
+     *      latAngle - '0'
+     */
+
+    double altitude = 0;
+    double longtitude;
+    double latitude;
+
+    double lonAngle = 0;
+    double latAngle = 0;
+
+    double height;
+
+
+
+
+    // Power parameters
+    double maxPowerInDbm;
+
+    double pathLoss = 0; //to calculate
+
+
+    // Frequency parameters
+    int ARFCN = 0;
+    int numerologyIndex = 0;
+    int SCS = NUMEROLOGY[numerologyIndex];
+    int bandwidth;
+    int carrierFrequency; // in Mhz
+
+
+
+
+public:
+    Equipment(){
+        this->bandwidth = 10;
+    }
+
+    void setLatitude(double lat){
+        this->latitude = lat;
+    }
+
+    void setLontitude(double lon){
+        this->longtitude = lon;
+    }
+
+    void setAltitude(double alt){
+        this->altitude = alt;
+    }
+
+};
+
+#endif // EQUIPMENT_H
