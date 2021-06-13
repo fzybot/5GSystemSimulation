ee5dda7b779b48cd74a6b90c65fa8cc439486347
fzybot <ruslan.akhpashev@gmail.com>
2021-06-14 00:16:05 +0700

    add doxygen

---
+++ docs/html/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['loadfile_1',['loadFile',['../classMainWindow.html#aa08469ed5c8396e60fa44bae6530bcf1',1,'MainWindow']]]
+];
---
+++ docs/html/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['closeevent_4',['closeEvent',['../classMainWindow.html#a05fb9d72c044aa3bb7d187b994704e2f',1,'MainWindow']]]
+];
