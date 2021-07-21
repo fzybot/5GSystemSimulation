cd908f489401919c0ea7a7f33f797ef186ed0154
fzybot <ruslan.akhpashev@gmail.com>
2021-07-21 16:02:26 +0700

    3d plot is working

--- src/visualization/Custom3dSurface.cpp
+++ src/visualization/Custom3dSurface.cpp
@@ -31,6 +31,29 @@ Custom3dSurface::~Custom3dSurface()
     delete graph_;
 }
 
+void Custom3dSurface::fillSqrtSinProxy()
+{
+    float stepX = (sampleMax - sampleMin) / float(sampleCountX - 1);
+    float stepZ = (sampleMax - sampleMin) / float(sampleCountZ - 1);
+
+    QtDataVisualization::QSurfaceDataArray *dataArray = new QtDataVisualization::QSurfaceDataArray;
+    dataArray->reserve(sampleCountZ);
+    for (int i = 0; i < sampleCountZ; i++) {
+        QtDataVisualization::QSurfaceDataRow *newRow = new QtDataVisualization::QSurfaceDataRow(sampleCountX);
+        float z = qMin(sampleMax, (i * stepZ + sampleMin));
+        int index = 0;
+        for (int j = 0; j < sampleCountX; j++) {
+            float x = qMin(sampleMax, (j * stepX + sampleMin));
+            float R = qSqrt(z * z + x * x) + 0.01f;
+            float y = (qSin(R) / R + 0.24f) * 1.61f;
+            (*newRow)[index++].setPosition(QVector3D(x, y, z));
+        }
+        qDebug() << "New row: " << newRow->size();
+        *dataArray << newRow;
+    }
+    sqrtSinProxy_->resetArray(dataArray);
+}
+
 void Custom3dSurface::enableSqrtSinModel(bool enable)
 {
     if (enable) {
@@ -55,52 +78,4 @@ void Custom3dSurface::enableSqrtSinModel(bool enable)
         stepZ_ = (sampleMax - sampleMin) / float(sampleCountZ - 1);
     }
 }
-
-void Custom3dSurface::fillSqrtSinProxy()
-{
-    float stepX = (sampleMax - sampleMin) / float(sampleCountX - 1);
-    float stepZ = (sampleMax - sampleMin) / float(sampleCountZ - 1);
-
-    QtDataVisualization::QSurfaceDataArray *dataArray = new QtDataVisualization::QSurfaceDataArray;
-    dataArray->reserve(sampleCountZ);
-    for (int i = 0; i < sampleCountZ; i++) {
-        QtDataVisualization::QSurfaceDataRow *newRow = new QtDataVisualization::QSurfaceDataRow(sampleCountX);
-        float z = qMin(sampleMax, (i * stepZ + sampleMin));
-        int index = 0;
-        for (int j = 0; j < sampleCountX; j++) {
-            float x = qMin(sampleMax, (i * stepX + sampleMin));
-            float R = qSqrt(z * z + x * x) + 0.01f;
-            float y = (qSin(R) / R + 0.24f) * 1.61f;
-            (*newRow)[index].setPosition(QVector3D(x, y, z));
-        }
-        *dataArray << newRow;
-    }
-    sqrtSinProxy_->resetArray(dataArray);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
