bcbed4bb11c6d9b7bfb7dd0bc48bba059ce1f0bb
fzybot <ruslan.akhpashev@gmail.com>
2021-08-05 14:27:35 +0700

    class Signal is updated for plot showing

--- src/protocols/phy/Signal.cpp
+++ src/protocols/phy/Signal.cpp
@@ -2,11 +2,100 @@
 
 #include <QDebug>
 #include <QRandomGenerator>
+#include <QMath>
 
+// ----- [ CONSTRUCTORS\DESTRUCTORS ] ----------------------------------------------------------------------------------
 
-Signal::Signal()
+Signal::Signal(QObject *parent):
+    QObject(parent),
+    index_(-1)
 {
 
 }
 
+// ----- [ PLOT FUNCTIONS ] --------------------------------------------------------------------------------------------
 
+void Signal::update(QtCharts::QAbstractSeries *series, int seriesIndex)
+{
+    if (series) {
+        QtCharts::QXYSeries *xySeries = static_cast<QtCharts::QXYSeries *>(series);
+        const QVector<QVector<QPointF>> &seriesData = data_.at(seriesIndex);
+        if (seriesIndex == 0) {
+            index_++;
+        }
+        if (index_ > seriesData.count() - 1) {
+            index_ = 0;
+        }
+
+        QVector<QPointF> points = seriesData.at(index_);
+        xySeries->replace(points);
+    }
+}
+
+void Signal::handleSceneChanged()
+{
+    dataUpdater_.start();
+}
+
+void Signal::updateAllSeries()
+{
+    static int frameCount = 0;
+    static QString labelText = QStringLiteral("FPS: %1");
+
+    for (int i = 0; i < seriesList_.size(); i++)
+        update(seriesList_[i], i);
+
+    frameCount++;
+    int elapsed = fpsTimer_.elapsed();
+    if (elapsed >= 1000) {
+        elapsed = fpsTimer_.restart();
+        qreal fps = qreal(0.1 * int(10000.0 * (qreal(frameCount) / qreal(elapsed))));
+        fpsLabel_->setText(labelText.arg(QString::number(fps, 'f', 1)));
+        fpsLabel_->adjustSize();
+        frameCount = 0;
+    }
+}
+
+void Signal::startUpdates(const QList<QtCharts::QXYSeries *> &seriesList, QLabel *fpsLabel)
+{
+    seriesList_ = seriesList;
+    fpsLabel_ = fpsLabel;
+
+    dataUpdater_.setInterval(0);
+    dataUpdater_.setSingleShot(true);
+    QObject::connect(&dataUpdater_, &QTimer::timeout,
+                     this, &Signal::updateAllSeries);
+
+    fpsTimer_.start();
+    updateAllSeries();
+}
+
+void Signal::generateData(int seriesCount, int rowCount, int colCount)
+{
+    data_.clear();
+
+    qreal xAdjustment = 20.0 / (colCount * rowCount);
+    qreal yMultiplier = 3.0 / qreal(seriesCount);
+
+    // Append the new data depending on the type
+    for (int k(0); k < seriesCount; k++) {
+        QVector<QVector<QPointF> > seriesData;
+        qreal height = qreal(k) * (10.0 / qreal(seriesCount)) + 0.3;
+        for (int i(0); i < rowCount; i++) {
+            QVector<QPointF> points;
+            points.reserve(colCount);
+            for (int j(0); j < colCount; j++) {
+                qreal x(0);
+                qreal y(0);
+                // data with sin + random component
+                y = height + (yMultiplier * qSin(M_PI / 50 * j)
+                              + (yMultiplier * QRandomGenerator::global()->generateDouble()));
+                // 0.000001 added to make values logaxis compatible
+                x = 0.000001 + 20.0 * (qreal(j) / qreal(colCount)) + (xAdjustment * qreal(i));
+                points.append(QPointF(x, y));
+            }
+            seriesData.append(points);
+        }
+        data_.append(seriesData);
+    }
+}
--- src/protocols/phy/Signal.h
+++ src/protocols/phy/Signal.h
@@ -3,16 +3,44 @@
 
 #include <QVector>
 #include <QString>
+#include <QtCore/QObject>
+#include <QtCharts/QXYSeries>
+#include <QtWidgets/QLabel>
+#include <QtCore/QElapsedTimer>
+#include <QtCore/QTimer>
 
 
-class Signal
+class Signal : public QObject
 {
-public:
-// ----- [ CONSTRUCTORS ] ----------------------------------------------------------------------------------------------
-    Signal();
-    Signal(int sampleRate, int durationTime);
+    Q_OBJECT
+private:
+    int sampleRate_;
+    int FFTSize_;
 
-// ----- [ SETTERS\GETTERS ] ----------------------------------------------------------------------------------------------
+    QVector<QVector<double>> powerValues_;
+    QVector<double> signalInTime_;
+    QVector<QVector<double>> IOvalues_;
+
+    QVector<QVector<QVector<QPointF> > > data_;
+    int index_;
+    QList<QtCharts::QXYSeries *> seriesList_;
+    QLabel *fpsLabel_;
+    QElapsedTimer fpsTimer_;
+    QTimer dataUpdater_;
+
+public slots:
+    void generateData(int seriesCount, int rowCount, int colCount);
+    void update(QtCharts::QAbstractSeries *series, int seriesIndex);
+    void handleSceneChanged();
+    void updateAllSeries();
+
+public:
+// ----- [ CONSTRUCTORS\DESTRUCTORS ] ----------------------------------------------------------------------------------
+    Signal(QObject *parent = 0);
+    virtual ~Signal() = default;
+
+
+// ----- [ SETTERS\GETTERS ] -------------------------------------------------------------------------------------------
     void setPowerValues(const QVector<QVector<double>>& powerValues);
     void setIOValues(const QVector<QVector<double>>& IOvalues);
     void getDataFromFile(QString filePath);
@@ -23,13 +51,7 @@ public:
     void printIOValues() const;
     void generateRandomIOValues(int MIMOSize, int dataSize);
 
-    virtual ~Signal() = default;
+// ----- [ PLOT FUNCTIONS ] --------------------------------------------------------------------------------------------
+    void startUpdates(const QList<QtCharts::QXYSeries *> &seriesList, QLabel *fpsLabel);
 
-private:
-    int sampleRate_;
-    int FFTSize_;
-
-    QVector<QVector<double>> powerValues_;
-    QVector<double> signalInTime_;
-    QVector<QVector<double>> IOvalues_;
 };
