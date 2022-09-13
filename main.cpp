91f4bf723ccb37cca397eaa4395548546f945588
t1m0f3y <t.leonenko2001@gmail.com>
2022-09-13 16:31:09 +0700

    some changes

--- main.cpp
+++ main.cpp
@@ -63,9 +63,9 @@ int main(int argc, char *argv[])
     mainWin.show();
 
 
-    Simple();
-    propagation_model_test_plot();
-    test_passing();
+//    Simple();
+//    propagation_model_test_plot();
+//    test_passing();
 
     //##############################################################################
     //                   Testing features (in src/scenarios/tests.h)
